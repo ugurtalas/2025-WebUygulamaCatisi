40c9224acfc4eae2366b8d6a3c4a6c36796189e0
Uğur TALAŞ <130050855+ugurtalas@users.noreply.github.com>
2025-02-27 08:37:10 +0300

    MVC Örneği
    2. hafta işlenen MVC örneği gerçekleştirildi

---
+++ Projeler/MVCOrnekProje/MVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult IlkMetot()
+        {
+            return View();
+        }
+
+        public IActionResult FarkliViewAc()
+        {
+            return View("IkinciView");
+        }
+
+        public IActionResult FarkliKlasordenViewAc()
+        {
+            return View("/Views/Ornek/OrnekView.cshtml");
+        }
+
+        public IActionResult ModelKullananMetot()
+        {
+            Models.IlkModel Nesne = new IlkModel();
+            Nesne.Ad = "Osman";
+            Nesne.Soyad = "Hasan";
+            _logger.LogInformation("Nesne  baþarýlý þekilde oluþtu.");
+            return View("ModelKullananView",Nesne);
+        }
+
+        //ilk controller
+        //ilk view
+        //farklý view
+        //logger
+        //ilk model
+        // view de model kullanýmý
+        // Nesne yada liste örneði
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projeler/MVCOrnekProje/MVC/Models/IlkModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models
+{
+    public class IlkModel
+    {
+        public int No { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
